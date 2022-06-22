1d2df34c317f3978c4dab03ce8dcc1d6c8568184
Sarkorbek Abdullaev <sarkorbek7777@gmail.com>
2022-06-22 16:52:24 +0500

    added

---
+++ script.js
@@ -0,0 +1,25 @@
+/* var a = prompt('salom so`zini kiriting ');
+
+if (a == "salom") {
+    console.log('sizga ham salom');
+} else {
+    console.log('salom qani');
+} */
+
+/* var b = +prompt('5 raqamini kiriting');
+if (b === 5) {
+    console.log('qiymat to`g`ri');
+} else {
+    console.log('qiymat xato');
+} */
+
+var age = prompt("Yoshingiz nechida?");
+if (age < 20) {
+    console.log('siz yoshsiz o`qing');
+} else if (age < 40) {
+    console.log('siz ishlang');
+} else if (age < 50) {
+    console.log('siz dam oling');
+} else {
+    console.log('Siz pensiya yoshidasiz, borib pensiyangizni oling');
+}
